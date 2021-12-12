f7b1934b22c692c54bd5d982bd262b5f7c26e85c
QiZhang-UConn <Qi.Zhang@uconn.edu>
2021-12-12 17:45:27 -0500

    basic login functionality

---
+++ src/main/java/com/revature/lemon/user/AuthController.java
@@ -0,0 +1,38 @@
+package com.revature.lemon.user;
+
+
+import com.revature.lemon.user.dtos.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    @Autowired
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void authenticate(@RequestBody LoginRequest loginRequest, HttpSession httpSession) {
+        User authUser = userService.login(loginRequest);
+        httpSession.setAttribute("authUser", authUser);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void invalidateSession(HttpSession session) {
+        session.invalidate();
+    }
+
+
+
+}
---
+++ src/main/java/com/revature/lemon/user/dtos/LoginRequest.java
@@ -0,0 +1,34 @@
+package com.revature.lemon.user.dtos;
+
+public class LoginRequest {
+    private String id;
+    private String username;
+    private String discriminator;
+
+
+    public LoginRequest(String id, String username, String discriminator){
+        this.id=id;
+        this.username=username;
+        this.discriminator=discriminator;
+    }
+
+    @Override
+    public String toString() {
+        return "New User Information{"+
+                "id='" + id + '\'' +
+                "username='" + username+ '\'' +
+                "discriminator='" + discriminator + '\'' +
+                "}";
+    }
+
+    //____________________________________Getter Section____________________________________
+    public String getId() {return id;}
+    public String getUsername() {return username;}
+    public String getDiscriminator() {return discriminator;}
+    //____________________________________Setter Section____________________________________
+    public void setId(String id) {this.id = id;}
+    public void setUsername(String username) {this.username = username;}
+    public void setDiscriminator(String discriminator) {this.discriminator = discriminator;}
+
+
+}
